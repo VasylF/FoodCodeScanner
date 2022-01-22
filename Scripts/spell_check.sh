21eb580bfe1e56a14958a952a87dc46727613962
Vasyl <39911739+VasylF@users.noreply.github.com>
2022-01-22 02:18:32 +0200

    Update spell_check.sh

--- Scripts/spell_check.sh
+++ Scripts/spell_check.sh
@@ -1,47 +1,26 @@
 
 #!/bin/sh
-if which typokana >/dev/null; then
-  typokana --diff-only
-else
-  echo "`typokana` is not installed."
+# if which typokana >/dev/null; then
+#   typokana --diff-only
+# else
+#   echo "`typokana` is not installed."
+# fi
+if ! [ -f /usr/local/bin/SpellChecker ]; then
+    echo "SpellChecker not installed"
+    exit 0
 fi
 
-# YELLOW='\033[1;33m'
-# NOCOLOR='\033[0m'
-# RED='\033[0;31m'
+git_path=/usr/local/bin/git
+files=$($git_path diff --diff-filter=d --name-only -- "*.swift" "*.h" "*.m")
+if (test -z $files) || (test ${#files[@]} -eq 0); then
+  echo "no files changed."
+  exit 0
+fi
 
-# OUT_PUT_FILE_NAME=temp_file
+options=""
+for file in $files
+do
+  options="$options $SRCROOT/$file"
+done
 
-# rm $OUT_PUT_FILE_NAME
-
-# touch $OUT_PUT_FILE_NAME
-
-# echo "false" > $OUT_PUT_FILE_NAME
-
-# function processOutPut() {
-#   while read -r line
-#    do
-#      if [[ $line = *"Test"* ]]; then
-#        echo "${YELLOW}$line${NOCOLOR}"
-#      elif [[ $line == *"warning"* ]]; then
-#        echo "${RED}$line${NOCOLOR}"
-#        echo "true" > $1
-#      else
-#        echo "$line"
-#      fi
-#   done
-# }
-
-# export PATH=~/.mint/bin:$PATH
-
-# mint install ezura/spell-checker-for-swift@5.3.0
-
-# typokana | processOutPut $OUT_PUT_FILE_NAME
-
-# SHOULD_FAIL=$(cat $OUT_PUT_FILE_NAME)
-
-# rm $OUT_PUT_FILE_NAME
-
-# if [ "$SHOULD_FAIL" = true ]; then
-#   exit 1
-# fi
+/usr/local/bin/SpellChecker -- $options
